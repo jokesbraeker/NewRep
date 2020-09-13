81d1ed4dad10920e13959b3a3838d054fbf682fd
unknown <tpmalakkad@gmail.com>
2020-09-13 22:30:16 +0530

    Updates

---
+++ precache-manifest.ecba5659f8ba1dcf16e63394e6bea455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e513938e5a803d7b942a94700fe10c30",
+    "url": "/NewRep/index.html"
+  },
+  {
+    "revision": "b3d0080e9db38ac8e451",
+    "url": "/NewRep/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1c91b26d8b7c85c7edbe",
+    "url": "/NewRep/static/css/main.5d64566f.chunk.css"
+  },
+  {
+    "revision": "b3d0080e9db38ac8e451",
+    "url": "/NewRep/static/js/2.9f92e6e2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/NewRep/static/js/2.9f92e6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c91b26d8b7c85c7edbe",
+    "url": "/NewRep/static/js/main.128a76d4.chunk.js"
+  },
+  {
+    "revision": "0d2ae060c113bbff01e5",
+    "url": "/NewRep/static/js/runtime-main.5dd0501d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NewRep/static/media/logo.5d5d9eef.svg"
+  }
+]);
